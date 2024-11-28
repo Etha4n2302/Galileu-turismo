0839a302f183fa01d83b0e2637d8f4d5a16b4868
Etha4n2302 <ethanolivirasilva@gmail.com>
2024-11-28 18:58:31 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,59 +1,59 @@
 export const data = [
     {
       "titulo": "Praça da Independência",
-      "descricao": "A Praça da Independência é um marco histórico da cidade, rodeada por casarões antigos e iluminada com uma bela fonte e bancos confortáveis. Está localizada no coração de Espírito Santo do Pinhal.",
+      "descricao": "A Praça da Independência é um verdadeiro tesouro histórico no coração de Espírito Santo do Pinhal. Cercada por imponentes casarões antigos, ela encanta com sua atmosfera acolhedora, repleta de charme. À noite, a praça ganha vida com uma deslumbrante fonte iluminada, convidando os visitantes a relaxar nos bancos confortáveis e apreciar a tranquilidade do lugar. Um destino imperdível para quem deseja vivenciar a história e a beleza desta cidade encantadora!",
       "imagem": "images/praca-independencia.jpg",
-      "categorias": ["Histórico", "Cultural"]
+      "categorias": ["Histórico"]
     },
   
 
         {
           "titulo": "Vinícola Guaspari",
-          "descricao": "Vinícola que oferece visitas guiadas pelos vinhedos e degustação de vinhos. A visita inclui passeio pela indústria e o Wine Bar com vista para os vinhedos.",
+          "descricao": "Descubra o encanto da vinícola que proporciona uma experiência única para os amantes de vinho! Aproveite as visitas guiadas pelos exuberantes vinhedos e mergulhe no processo de produção com um tour pela indústria. A experiência ainda conta com uma parada especial no Wine Bar, onde você pode degustar vinhos de excelência enquanto desfruta de uma vista deslumbrante para os vinhedos. Uma experiência inesquecível para os sentidos!",
           "imagem": "images/foto guaspari.jpg",
-          "categorias": ["Ecoturismo", "Degustação Sensoriais","Vinhos"]
+          "categorias": ["Ecoturismo", "Degustação","Vinhos"]
         },
         
           {
             "titulo": "Apiário Maria Mel",
             "descricao": "O apiário oferece vivências relacionadas à criação de abelhas e produção de mel, além de palestras sobre apicultura. Equipamentos de proteção são fornecidos para visitação.",
             "imagem": "images/apiario-maria-mel.jpg",
-            "categorias": ["Natureza", "Educação"]
+            "categorias": ["Ecoturismo"]
           },
           {
             "titulo": "Palácio do Café",
             "descricao": "Inaugurado em 1893, o Palácio do Café é um símbolo da história do café em Espírito Santo do Pinhal, atualmente sede do governo municipal e do Centro Cultural.",
             "imagem": "images/palacio.jpg",
-            "categorias": ["Histórico", "Cultural","Educação","Arquitetura"]
+            "categorias": ["Histórico","Arquitetura"]
           },
           {
             "titulo": "Fazenda Mocoquinha",
             "descricao": "Fazenda histórica que oferece trilhas ecológicas e preserva árvores milenares. Um local ideal para o ecoturismo e a conscientização ambiental.",
             "imagem": "images/fazenda-mocoquinha.jpg",
-            "categorias": ["Natureza", "Ecoturismo"]
+            "categorias": [ "Ecoturismo"]
           },
           {
             "titulo": "Casarão 175",
             "descricao": "Construído em 1903, o Casarão 175 é um exemplo da arquitetura do final do século XIX e atualmente funciona como um centro cultural.",
             "imagem": "images/casarao-175.jpg",
-            "categorias": ["Histórico", "Arquitetura","Educação"]
+            "categorias": ["Histórico", "Arquitetura"]
           },
           {
             "titulo": "Rancho Rio Manso",
             "descricao": "Espaço rural que oferece lazer em contato com a natureza, promovendo passeios e conexão com o ambiente natural.",
             "imagem": "images/rancho-rio-manso.jpg",
-            "categorias": ["Natureza", "Ecoturismo"]
+            "categorias": ["Ecoturismo",]
           },
           {
             "titulo": "Cristo Redentor",
             "descricao": "Estátua de 11 metros de altura inspirada no Cristo Redentor do Rio de Janeiro, localizada em um dos pontos mais altos da cidade.",
             "imagem": "images/cristo.png",
-            "categorias": ["Religioso", "Turístico","Arquitetura"]
+            "categorias": ["Religioso","Arquitetura"]
           },
           {
             "titulo": "Santuário Santa Luzia",
             "descricao": "A Igreja de Santa Luzia localiza-se no bairro rural de mesmo nome na cidade de Espírito Santo do Pinhal/SP.A região onde se situa era conhecida – em fins do século XIX – como Bairro Morro Azul.",
-            "imagem": "images/santa-luzia.png",
+            "imagem": "images/santa luiza.png",
             "categorias": ["Religioso"]
 
           },
@@ -61,39 +61,39 @@ export const data = [
             "titulo": "Igreja matriz",
               "descricao":"Igreja Matriz do Divino Espírito Santo, praça central de Espírito Santo do Pinhal. Lindíssima, com belas pinturas, espaço amplo e belos altares em madeira. ",
               "imagem": "images/igreja matriz.jpg",
-              "categorias":["Religioso", "Turístico","Arquitetura"]
+              "categorias":["Religioso","Arquitetura"]
           },
         
             {
               "titulo": "Terra de kurí",
               "descricao":" O Terra de Kurí, no Espírito Santo Do Pinhal, oferece acomodações apenas para adultos com jardim e bar.",
               "imagem": "images/Terra de kurí.jpg",
-              "categorias": ["Pousada", "Turístico","Educação"]
+              "categorias": ["Pousada",]
             },
 
             {
               "titulo": "Lago municipal",
               "descricao": "Localizado em uma área privilegiada no município, o Lago Municipal é uma das principais atrações de Espírito Santo do Pinhal. ",
               "imagem": "images/lago municipal.jpg",
-              "categorias":["Natureza", "Turístico"]
+              "categorias":["Ecoturismo"]
               },
               {
                 "titulo": "Inverno D' Itália",
                 "descricao": " Uma rede de cafeterias pinhalense, focada nos cafés especiais de Espírito Santo do Pinhal, com deliciosos doces e salgados produzidos em confeitaria própria.",
                 "imagem": "images/inverno de italia.png",
-                "categorias": ["Degustação Sensoriais", "Cafeteria"]
+                "categorias": ["Degustação", "Cafeteria"]
               },
               {
                 "titulo": "Casa do Ipê",
                 "descricao": "Uma chácara dentro da cidade que abriga o segundo vinhedo mais antigo de Pinhal, além de sua culinária impecavél.",
                 "imagem": "images/casa do ipe.png",
-                "categorias": ["Degustação Sensoriais","Vinhos"]
+                "categorias": ["Degustação","Vinhos"]
               },
               {
                 "titulo": "Toca do Kaynã",
                 "descricao": "Fazenda localizada aos pés da Serra da Mantiqueira paulista, com experiências sem iguais.",
                 "imagem": "images/toca kayna.png",
-                "categorias": ["Ecoturismo","Degustação Sensoriais","Educação"]
+                "categorias": ["Ecoturismo","Degustação"]
               },
 
           
